3a3ae2625c9aceaf3b54a42694b5fb12e3d68e5f
Manuel E. Hernandez <kicke10h@gmail.com>
2022-06-19 14:28:05 -0500

    Sixth exercises

---
+++ p-6/README.md
@@ -0,0 +1,6 @@
+# Exercise 6
+
+Write a program that helps the user to store some books and their author in the
+notes.json file. The program will contain functions to load Notes, save Notes and
+addNotes. Import the js file as a module inside another source file and
+use the functions there. Use the read and write methods to work with the data.
---
+++ p-6/index.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+let Book = class {
+    constructor(name_book, author) {
+        this.name_book = name_book;
+        this.author = author;
+    }
+};
+
+//Initialize the notes.json
+try {
+    if(!fs.existsSync('./notes.json')) {
+        fs.writeFileSync('./notes.json', '[]')
+    }
+}
+catch (err) {
+    console.log(err);
+}
+
+
+//Function to read from console
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const question = (str) => new Promise(resolve => readline.question(str, data => {
+    resolve(data);
+}));
+
+opt = menu();
+
+async function menu () {
+    let books = [];
+    let opt = 0;
+    while (opt != 4) {
+        console.log('1. Load Book'); 
+        console.log('2. Save Book'); 
+        console.log('3. Add Book');
+        console.log('4. Exit');
+        opt = parseInt(await question('Select an option: '));
+        switch (opt) {
+            case 1:
+                console.log('\033[2J');
+                console.log(JSON.parse(fs.readFileSync('./notes.json', 'utf8')));
+                await question('Press any key to continue...')
+                console.log('\033[2J');
+                break;
+            case 2:
+                console.log('\033[2J');
+                let name_book = await question("Name book: ")
+                let author = await question("Author: ")
+                books.push(new Book(name_book, author))
+                await question('Press any key to continue...')
+                console.log('\033[2J');
+                break;
+            case 3:
+                console.log('\033[2J');
+                for(b in books) {
+                    let book = books[b];
+                    console.log(`${parseInt(b)+1}.- Name: ${book.name_book}; Author: ${book.author}`);
+                }
+                let notes = JSON.parse(fs.readFileSync('./notes.json', 'utf8'));
+                let index = parseInt(await question('Select book to save: '))-1;
+                notes.push(books[index]);
+                books.pop(index);
+                fs.writeFileSync('./notes.json', JSON.stringify(notes))
+                await question('Press any key to continue...')
+                console.log('\033[2J');
+                break;
+        }
+    }
+    readline.close();
+}
